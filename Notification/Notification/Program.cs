1314e3aa024c5bf84bdf2a5af819fee3052a6b8b
Ruksana BR <125458606+RUKSANABR@users.noreply.github.com>
2025-09-15 19:32:08 +0530

    Add files via upload

---
+++ Notification/Notification/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+// delegate
+public delegate void Download(string fileName);
+
+public class FileDownloader
+{
+    // event using deligate
+    public event Download DownloadCompleted;
+
+    // raise the event
+    public void DownloadFile(string fileName)
+    {
+        Console.WriteLine($"Downloading {fileName}...");
+        Thread.Sleep(1000);
+        Console.WriteLine($"{fileName} download finished.");
+
+        // Raise the event
+        DownloadCompleted?.Invoke(fileName);
+    }
+}
+
+// method to handles the event
+public class User
+{
+    public void DownloadCompleted(string fileName)
+    {
+        Console.WriteLine($"Notification: {fileName} has been downloaded.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Create objects
+        FileDownloader downloader = new FileDownloader();
+        User user = new User();
+
+        // Subscribe method to the event
+        downloader.DownloadCompleted += user.DownloadCompleted;
+
+        // Start download
+        downloader.DownloadFile("hello.pdf");
+    }
+}
